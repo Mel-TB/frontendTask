92f31a83a47f05c88657c1bd5ef868171b979fa2
Mel-TB <melinda.trami@live.fr>
2023-11-23 16:59:25 +0100

    test(redux-actions): Add tests for setWidgets, setActiveWidgets and setError

---
+++ src/store/actions/widgetActions.test.js
@@ -0,0 +1,32 @@
+import {
+  setWidgets,
+  setActiveWidget,
+  setError,
+} from "../../store/actions/widgetActions";
+
+test("setWidgets action", () => {
+  const widgets = [{ id: 1, name: "Test Widget" }];
+  const expectedAction = {
+    type: "SET_WIDGETS",
+    payload: widgets,
+  };
+  expect(setWidgets(widgets)).toEqual(expectedAction);
+});
+
+test("setActiveWidget action", () => {
+  const widgetId = 1;
+  const expectedAction = {
+    type: "SET_ACTIVE_WIDGET",
+    payload: widgetId,
+  };
+  expect(setActiveWidget(widgetId)).toEqual(expectedAction);
+});
+
+test("setError action", () => {
+  const error = new Error("Test Error");
+  const expectedAction = {
+    type: "SET_ERROR",
+    payload: error,
+  };
+  expect(setError(error)).toEqual(expectedAction);
+});
